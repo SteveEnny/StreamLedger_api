5c1348038c4e1d64788f48566263a85710e5b1c5
omotoso eniola <omotosoeniola2@gmail.com>
2025-10-25 18:27:37 +0100

    feat:add ngix conf

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,29 @@
 FROM richarvey/nginx-php-fpm:3.1.6
 
+WORKDIR /var/www/html
+
+# Copy only composer files first
+COPY composer.json composer.lock ./
+
+# Install PHP dependencies
+RUN composer install --no-dev --optimize-autoloader
+
+# Copy full application
 COPY . .
 
-# Image config
-ENV SKIP_COMPOSER 1
+# Run Laravel build commands
+RUN php artisan key:generate
+RUN php artisan config:cache
+RUN php artisan route:cache
+
+# Environment configuration
 ENV WEBROOT /var/www/html/public
 ENV PHP_ERRORS_STDERR 1
 ENV RUN_SCRIPTS 1
 ENV REAL_IP_HEADER 1
-
-# Laravel config
 ENV APP_ENV production
 ENV APP_DEBUG false
 ENV LOG_CHANNEL stderr
-
-# Allow composer to run as root
 ENV COMPOSER_ALLOW_SUPERUSER 1
 
 CMD ["/start.sh"]
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Transaction;
 use App\Observers\TransactionObserver;
 use Illuminate\Support\ServiceProvider;
 use App\Services\KafkaProducerService;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,8 +23,15 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+
+    public function boot(UrlGenerator $url)
     {
-        Transaction::observe(TransactionObserver::class);
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
     }
+    // public function boot(): void
+    // {
+    //     Transaction::observe(TransactionObserver::class);
+    // }
 }
